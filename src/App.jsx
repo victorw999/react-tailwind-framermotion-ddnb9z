f894827fbebb72460219bab0e9a8d12d3c928a4e
victorw999 <vicwong999@gmail.com>
2024-04-10 14:01:47 -0700

    framer motion basic demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import LeftPanel from './LeftPanel';
 import RightPanel from './RightPanel';
+import { motion, useInView } from 'framer-motion'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null)
+  const isInView = useInView(ref)
+
+  useEffect(() => {
+    console.log('===> red box is in view: ', isInView)
+  }, [isInView])
   return (
     <div>
       <small>tutorial: https://youtu.be/znbCa4Rr054?si=1AF_KgTeqa4AkJ81</small>
       <p className="text-red">isOpen: {isOpen.toString()} </p>
-      <div className="flex h-screen w-screen pr-20 border-2 border-red-500 border-solid  m-auto">
+      <div className="flex h-[40rem] min-w-[40rem]  border-2 border-red-500 border-solid  m-auto">
         <LeftPanel isOpen={isOpen} setIsOpen={setIsOpen} />
         <RightPanel isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
+      <div className="filler h-[150vh] bg-pink-100"></div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className=' flex h-[40rem] bg-gray-500 border-[5rem] border-black border-solid'>
+        <span className="text-white text-xl bold text-center w-full">reveal while in view</span>
+      </motion.div>
+
+      <div ref={ref}
+        className="bg-red-500 h-[40rem] transition duration-1000"></div>
     </div>
   );
 }
--- src/LeftPanel.jsx
+++ src/LeftPanel.jsx
@@ -1,46 +1,67 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, MotionConfig, useAnimationControls, AnimatePresence } from 'framer-motion';
 import { MyComponent } from './MyComponent';
 
 function LeftPanel({ isOpen, setIsOpen }) {
+  const flipControls = useAnimationControls()
+  const handleFlipClick = () => {
+    flipControls.start('flip')
+  }
   return (
     <div className="w-4/12 bg-gray-200 flex flex-col items-center justify-center">
-      <motion.button
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.9, rotate: '3deg' }}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded-none"
-        onClick={() => setIsOpen(!isOpen)}
-        layout
-      >
-        {isOpen ? 'Close Modal' : 'Open Modal'}
-      </motion.button>
-      <AnimatePresence mode="popLayout">
-        {isOpen ? (
-          <motion.div
-            key="animated_box"
-            initial={{
-              rotate: '0deg',
-              scale: 0,
-            }}
-            animate={{
-              rotate: '360deg',
-              scale: 1,
-              y: [0, 150, -150, -150, 0],
-            }}
-            exit={{ rotate: '0deg', scale: 0 }}
-            transition={{
-              duration: 1,
-              ease: 'backInOut',
-              times: [0, 0.25, 0.5, 0.85, 1],
-            }}
-            className="bg-blue-300 w-16 mx-0 my-10 text-white font-bold py-2 px-4  rounded-none aspect-square  flex align-center justify-center text-center "
-          >
-            box
-          </motion.div>
-        ) : (
-          ''
-        )}
-      </AnimatePresence>
+      <MotionConfig transition={{
+        duration: 0.5, ease: "easeInOut"
+      }}>
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.9, rotate: '3deg' }}
+          className={` hover:bg-blue-700 text-white 
+          font-bold py-2 px-4  rounded-none ${isOpen ? "bg-red-500" : "bg-blue-500"}`  }
+          onClick={() => setIsOpen(!isOpen)}
+          layout
+        >
+          {isOpen ? 'Close Modal' : 'Open Modal'}
+        </motion.button>
+
+        <button onClick={handleFlipClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded-none my-4">Flip it</button>
+
+        <AnimatePresence mode="popLayout">
+          {isOpen ? (
+            <motion.div
+              whileHover="randomTriggerText"
+              variants={{
+                initial: {
+                  rotate: '0deg',
+                  scale: 0,
+                },
+                // animate: {
+                //   rotate: '360deg',
+                //   scale: 1,
+                //   y: [0, 150, -150, -150, 0],
+                // },
+                exit: { rotate: '0deg', scale: 0 },
+                randomTriggerText: {
+                  duration: 0.1,
+                  x: [0, 15, -15, -15, 0],
+                },
+                flip: {
+                  rotate: '360deg',
+                  scale: 1,
+                  y: [0, 150, -150, -150, 0],
+                }
+              }}
+
+              initial="initial"
+              animate={flipControls} 
+              className="bg-blue-300 w-16 mx-0 my-10 text-white font-bold py-2 px-4  rounded-none aspect-square  flex align-center justify-center text-center "
+            >
+              box
+            </motion.div>
+          ) : (
+            ''
+          )}
+        </AnimatePresence>
+      </MotionConfig>
       {/* <MyComponent isVisible={isOpen} /> */}
     </div>
   );
